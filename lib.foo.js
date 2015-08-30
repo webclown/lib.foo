7ded33f3d08ce8a30d89b9aabd680d1dc00acbf4
Keystion <this-key@live.com>
2015-08-31 00:58:27 +0800

    Update lib.foo.js
    Date format

--- lib.foo.js
+++ lib.foo.js
@@ -28,3 +28,28 @@ function changeDocumentTitle (argument) {
         }, 0)
     }).appendTo($body);
 }
+
+// 将当前时间换成时间格式字符串
+var timestamp3 = 1403058804;
+var newDate = new Date();
+
+Date.prototype.format = function(format) {
+    var date = {
+        "M+": this.getMonth() + 1,
+        "d+": this.getDate(),
+        "h+": this.getHours(),
+        "m+": this.getMinutes(),
+        "s+": this.getSeconds(),
+        "q+": Math.floor((this.getMonth() + 3) / 3),
+        "S+": this.getMilliseconds()
+    };
+    if (/(y+)/i.test(format)) {
+        format = format.replace(RegExp.$1, (this.getFullYear() + '').substr(4 - RegExp.$1.length));
+    }
+    for (var k in date) {
+        if (new RegExp("(" + k + ")").test(format)) {
+            format = format.replace(RegExp.$1, RegExp.$1.length == 1 ? date[k] : ("00" + date[k]).substr(("" + date[k]).length));
+        }
+    }
+    return format;
+}
