a3b040f8d46ecedfb96d8ea6d656712e8080dcf4
Keystion <this-key@live.com>
2015-12-26 18:34:16 +0800

    Update lib.foo.js

--- lib.foo.js
+++ lib.foo.js
@@ -183,3 +183,21 @@ if (storageSupported && "tweets" in window.sessionStorage) {
         }
     });
 }
+
+
+Array.prototype.indexOf = function(val) {
+    for (var i = 0; i < this.length; i++) {
+        if (this[i] == val) return i;
+    }
+    return -1;
+};
+Array.prototype.remove = function(val) {
+    var index = this.indexOf(val);
+    if (index > -1) {
+        this.splice(index, 1);
+    }
+};
+
+var _arr = ['a', 'b', 'c'];
+
+_arr.remove('b');  // ['a', 'c']
